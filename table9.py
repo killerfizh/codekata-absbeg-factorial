847068840c6c5d32470c3e8a69b9706359c2a669
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-03-28 18:37:35 +0530

    Update table9.py

--- table9.py
+++ table9.py
@@ -1,7 +1,7 @@
 def Table9(n):
     tab = 9
     for i in range(1, n+1):
-        tab = tab*i
-        print(tab, end=" ")
+        temp = tab*i
+        print(temp, end=" ")
 num = int(input())
 Table9(num)
